43263018fc6bd8dc66281cf0aca2915174169ea1
Kostya1375 <Kostya1375@gmail.com>
2014-07-24 08:25:59 +0300

    diplom
    Commited Diplom project

---
+++ Diplom/Diplom/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Diplom
+{
+    public partial class Form1 : Form
+
+    {
+        Logic logic;
+        //string values;
+        public Form1()
+        {
+            InitializeComponent();
+            logic = new Logic();
+            panel1.Visible = true;
+            panel2.Visible = false;
+            panel3.Visible = false;
+            panel4.Visible = false;
+
+            
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel2.Visible = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //panel1.Visible = true;
+            panel2.Visible = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            panel3.Visible = true;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            panel3.Visible = false;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            panel4.Visible = true;
+            panel5.Visible = true;
+            //string answer = combobox1.selectedindex.tostring();
+            //logic.setanswer(answer);
+            //logic.setallparametrsmicro(0, combobox1.selectedindex);
+            //logic.setallparametrsmicro(1, combobox2.selectedindex);
+            //logic.setallparametrsmicro(2, combobox3.selectedindex);
+            //logic.setallparametrsmicro(3, combobox4.selectedindex);
+            //label3.text = " " + logic.settextlabel();
+            
+            
+            
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            panel4.Visible = true;
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Diplom/Diplom/Logic.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diplom
+{
+    public class Logic
+    {
+        string answer;
+        int[] microclimat_parametrs = new int[4];
+
+        public void SetAnswer(string a)
+        {
+            answer = a;
+        }
+
+        public string SetTextLabel()
+        {
+            if (microclimat_parametrs[0] == 2 & microclimat_parametrs[1] == 2 & microclimat_parametrs[2] == 2 & microclimat_parametrs[3] == 1)
+            {
+                answer = "Опасно";
+            }
+            if (microclimat_parametrs[0] == 2 & microclimat_parametrs[1] == 2 & microclimat_parametrs[2] == 2 & microclimat_parametrs[3] == 0)
+            {
+                answer = "Не опасно";
+            }
+            return answer;
+        }
+
+        public void SetAllParametrsMicro(int i, int param)
+        {
+            microclimat_parametrs[i] = param;
+        }
+    }
+}
